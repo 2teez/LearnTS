3b7ef3f769c63276c3d938775e8a89289efe929b
timothy adigun <adigun.timothy@gmail.com>
2026-02-19 17:32:05 +0000

    filled in some options avaliable

--- makets.sh
+++ makets.sh
@@ -5,6 +5,20 @@
 #
 function avaliable_options() {
    echo "Options Avaliable:"
+   echo "-d:    Delete a directory or a file."
+   echo "-g:    Make a generic Typescript file."
+   echo "-h:    Help Function called."
+   echo "-p:    Make a Typescript Project."
+   echo "-s:    Make a standalone ts file."
+   echo "-r:    Run a Typescript file to generate a js file."
+}
+
+function help() {
+    if [[ $# != 2 ]]; then
+        echo "${0}" [options] [file]
+        avaliable_options
+        exit 1
+    fi
 }
 
 if [[ $# != 2 ]]; then
@@ -12,3 +26,34 @@ if [[ $# != 2 ]]; then
     avaliable_options
     exit 1
 fi
+
+# function to check the file extention
+
+optstring="d:g:p:r:s:h"
+while getopts "${optstring}" opt; do
+    case "${opt}" in
+        d)
+            ;;
+        h)
+            help
+            ;;
+        g)
+            ;;
+        p)
+            filename="${OPTARG}"
+            # make project folder
+            mkdir "${filename}" && cd "${filename}"
+            npm init --yes
+            ;;
+        r)
+            ;;
+        s)
+            ;;
+        h)
+            ;;
+        *)
+            echo "${opt} Invalid option"
+            help
+            exit 1
+    esac
+done
