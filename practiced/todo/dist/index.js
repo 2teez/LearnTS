f7fc9e53009ef27d76006a43b7bbbb3a613d09df
timothy adigun <adigun.timothy@gmail.com>
2026-02-20 10:13:26 +0000

    autogenerated js files

--- practiced/todo/dist/index.js
+++ practiced/todo/dist/index.js
@@ -1,2 +1,16 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const todoItem_1 = require("./todoItem");
+const todoitemCollection_1 = require("./todoitemCollection");
+let todos = [
+    new todoItem_1.TodoItem(1, "Buy Flowers"),
+    new todoItem_1.TodoItem(2, "Get Shoes"),
+    new todoItem_1.TodoItem(3, "Collect Tickets"),
+    new todoItem_1.TodoItem(4, "Call Joe", true),
+];
+let collection = new todoitemCollection_1.TodoCollection("Adam", todos);
 console.clear();
-console.log("Tim's Todo List!");
+console.log(`${collection.userName}'s Todo List.`);
+let newId = collection.addTodo("Go for run");
+let todoItem = collection.getTodoById(newId);
+todoItem.printDetails();
