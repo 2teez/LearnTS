c0b35a08698169e3dfec98d4de5a3685e4c5cf55
timothy adigun <adigun.timothy@gmail.com>
2026-02-21 08:15:54 +0000

    applying types

--- practiced/todo/dist/index.js
+++ practiced/todo/dist/index.js
@@ -1,5 +1,6 @@
 import { TodoItem } from "./todoItem.js";
 import { TodoCollection } from "./todoitemCollection.js";
+import inquirer from "inquirer";
 let todos = [
     new TodoItem(1, "Buy Flowers"),
     new TodoItem(2, "Get Shoes"),
@@ -12,6 +13,27 @@ function showDetails() {
     console.log(`${collection.userName}'s Todo List (${todos.filter((item) => !item.complete).length} Todo)`);
     todos.forEach((item) => item.printDetails());
 }
+var Commands;
+(function (Commands) {
+    Commands["Add"] = "Add Task";
+    Commands["Quit"] = "Quit";
+})(Commands || (Commands = {}));
+function UserPrompt() {
+    console.clear();
+    showDetails();
+    inquirer
+        .prompt({
+        type: "list",
+        name: "commands",
+        message: "Choosen Options",
+    })
+        .then((answers) => {
+        switch (answers["commands"]) {
+            case Commands.Quit:
+                break;
+        }
+    });
+}
 /*console.clear();
 console.log(`${collection.userName}'s Todo List.`);
 
@@ -19,4 +41,4 @@ let newId = collection.addTodo("Go for run");
 let todoItem = collection.getTodoById(newId);
 todoItem.printDetails();
 */
-showDetails();
+UserPrompt();
