3058518b88485185f19a32efe917acbd32334257
timothy adigun <adigun.timothy@gmail.com>
2026-02-21 21:21:29 +0000

    using object in js and ts

--- practiced/primer/dist/index.js
+++ practiced/primer/dist/index.js
@@ -12,4 +12,22 @@ let totalPrice = hatPrice + bootsPrice;
 console.log(`Total Price is ${totalPrice}.`);
 let myVariable = "Adam";
 console.log(`Type: ${typeof myVariable}`);
+// using objects
+let hat = {
+    name: "Hat",
+    _price: 100,
+    priceIncTax: 100 * 1.2,
+    set price(newPrice) {
+        this._price = newPrice;
+        this.priceIncTax = this._price * 1.2;
+    },
+    get price() {
+        return this._price;
+    },
+    writeDetails() {
+        console.log(`${this.name}, ${this.price} ${this.priceIncTax}`);
+    },
+};
+//hat.writeDetails.bind(hat);
+hat.writeDetails();
 export {};
--- practiced/primer/src/index.ts
+++ practiced/primer/src/index.ts
@@ -12,3 +12,25 @@ console.log(`Total Price is ${totalPrice}.`);
 
 let myVariable = "Adam";
 console.log(`Type: ${typeof myVariable}`);
+// using objects
+let hat = {
+  name: "Hat",
+  _price: 100,
+  priceIncTax: 100 * 1.2,
+
+  set price(newPrice) {
+    this._price = newPrice;
+    this.priceIncTax = this._price * 1.2;
+  },
+
+  get price() {
+    return this._price;
+  },
+
+  writeDetails() {
+    console.log(`${this.name}, ${this.price} ${this.priceIncTax}`);
+  },
+};
+
+//hat.writeDetails.bind(hat);
+hat.writeDetails();
