c69e643198f6d6236951d58e7a5bec61f28dcd33
timothy adigun <adigun.timothy@gmail.com>
2026-02-21 21:12:16 +0000

    testing some js syntax

--- practiced/primer/src/index.ts
+++ practiced/primer/src/index.ts
@@ -1,2 +1,14 @@
-console.clear()
-console.log("Start Here!")
+let hatPrice = 100;
+console.log(`Hat Price: ${hatPrice}`);
+let bootsPrice = 100;
+console.log(`Hat Price: ${bootsPrice}`);
+if (hatPrice === bootsPrice) {
+  console.log(`hatPrice and bootsPrice is the same.`);
+} else {
+  console.log(`hatPrice and bootsPrice is NOT the same.`);
+}
+let totalPrice = hatPrice + bootsPrice;
+console.log(`Total Price is ${totalPrice}.`);
+
+let myVariable = "Adam";
+console.log(`Type: ${typeof myVariable}`);
