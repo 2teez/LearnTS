9a0d007ea1d2b09ad5029a721987dd536af95bbd
timothy adigun <adigun.timothy@gmail.com>
2026-02-21 22:00:28 +0000

    using object prototype in js

---
+++ practiced/primer2/src/index.ts
@@ -0,0 +1,17 @@
+let hat = {
+  name: "Hat",
+  price: 100,
+  getPriceIncTax() {
+    return Number(this.price) * 1.2;
+  },
+};
+
+console.log(`Hat: ${hat.price}, ${hat.getPriceIncTax()}`);
+console.log(`${hat.toString()}`);
+
+let hatPrototype = Object.getPrototypeOf(hat);
+hatPrototype.toString = function () {
+  return `toString: ${this.name}: ${this.price}, ${this.getPriceIncTax()}`;
+};
+
+console.log(`${hat.toString()}`);
